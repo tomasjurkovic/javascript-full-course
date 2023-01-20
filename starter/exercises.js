11dd3b47cc47f68026a60273d0b687c2ff2390af
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-01-20 10:41:49 +0100

    creating separate file for exercises

--- starter/exercises.js
+++ starter/exercises.js
@@ -1,41 +1,11 @@
-/* FIRST LECTION:
-
-let js = 'amazing';
-
-console.log(40 + 8 + 23 - 10);
-console.log(23);
-
-let firstName = "Bob";
-console.log(firstName);
-
-const PI = 3.1415; // convention
-
-let myFirstJob = 'manual tester';
-let myCurrentJob = 'automation tester';
-
 // assigment:
 // 1. Declare variables called 'country', 'continent' and 'population' and
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
 
-*/
-
-// VALUES:
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
-
-console.log(typeof javascriptIsFun) // boolean
-
-// lets reasign the variable
-javascriptIsFun = "Yes!";
-console.log(typeof javascriptIsFun) // string
-
-// undefined variable>
-let year;
-console.log(year);
-console.log(typeof year);
-year = 1993;
-console.log(typeof year);
+let country = 'Slovakia';
+let continent = 'Europe';
+let population = 5.3;
 
 // assignment 2:
 /* 1. Declare a variable called 'isIsland' and set its value according to your
@@ -50,5 +20,3 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
-
-// Declaring variables:
