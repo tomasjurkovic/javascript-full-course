37efcfe7e694ac149cd7a275de7def772dfee86e
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-01-25 14:48:27 +0100

    added strict mode part

---
+++ starter-02/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// put it always in the first line of our code
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+    hasDriversLicense = true;
+}
+
+if (hasDriversLicense) console.log('I can drive');
+
+// reserved word: interface
+// const interface = 'Audio';
