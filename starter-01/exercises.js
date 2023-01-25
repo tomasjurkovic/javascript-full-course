9365b3457e247a5f52927a3c8e7600fef1008a89
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-01-25 14:57:44 +0100

    Update exercises.js
    added forgotten exercise for ternary operator

--- starter-01/exercises.js
+++ starter-01/exercises.js
@@ -157,3 +157,14 @@ switch (language) {
         break;
 }
 
+/* ASSIGNMENT 10:
+1. If your country's population is greater than 33 million, use the ternary operator
+to log a string like this to the console: 'Portugal's population is above average'.
+Otherwise, simply log 'Portugal's population is below average'. Notice how only
+one word changes between these two sentences!
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original
+*/
+
+const countryPopulationFacingAverage = population > averageCountryPopulation ? "Slovakia's population is above average" : "Slovakia's population is below average";
+console.log(countryPopulationFacingAverage);
