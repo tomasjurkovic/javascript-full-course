b42b4be541b433aa8be295115e6e45486b91bfea
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-01 12:24:45 +0100

    Update assignments.js
    challenge for arrays looping added

--- starter-02/assignments.js
+++ starter-02/assignments.js
@@ -206,6 +206,42 @@ console.log(myCountry.isIsland);
 Use a for loop to simulate the 50 people voting, by logging a string like this to
 the console (for numbers 1 to 50): 'Voter number 1 is currently voting */
 
-for (let voter = 1; voter <= 50; voter++) {
+for (let voter = 1; voter <= 5; voter++) {
     console.log(`Voter number ${voter} is currently voting`);
+}
+
+// LECTURE: Looping Arrays, Breaking and Continuing
+/* 1. Let's bring back the 'populations' array from a previous assignment
+2. Use a for loop to compute an array called 'percentages2' containing the
+percentages of the world population for the 4 population values. Use the
+function 'percentageOfWorld1' that you created earlier
+3. Confirm that 'percentages2' contains exactly the same values as the
+'percentages' array that we created manually in the previous assignment,
+and reflect on how much better this solution is */
+
+const percentagesUsingForLoop = [];
+for (let index = 0; index < populations.length; index++) {
+    percentagesUsingForLoop.push(percentageOfWorld1(populations[index]));
+}
+
+console.log(percentages);
+console.log(percentagesUsingForLoop);
+
+// LECTURE: Looping Backwards and Loops in Loops
+/* 1. Store this array of arrays into a variable called 'listOfNeighbours'
+[['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+'Russia']];
+2. Log only the neighbouring countries to the console, one by one, not the entire
+arrays. Log a string like 'Neighbour: Canada' for each country
+3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't
+worry if it's too difficult for you! But you can still try to figure this out anyway � */
+
+const listOfNeighbours =
+    [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden',
+        'Russia']];
+
+for (let index = 0; index < listOfNeighbours.length; index++) {
+    for (let j = 0; j < listOfNeighbours[index].length; j++) {
+        console.log(`Neighbour: ${listOfNeighbours[index][j]}`)
+    }
 }
