1fcca1b04540157672e6ad3f7f77ce5f355aa578
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-03 14:59:54 +0100

    Update script.js
    solved a exercise of calc amplitude

--- starter-03/script.js
+++ starter-03/script.js
@@ -10,4 +10,54 @@ const calcAge = birthyear => 2023 - birthyear;
 
 console.log(calcAge(1980));
 
-// TODO: BUG:
+// find out temperature amplitude:
+const temperatures = [3, -20, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [-77, -44, -47, -50, 'no result', -78, -90];
+const temperatures3 = [15, -23, -10, -15, 'no result', -7, -9];
+const temperatures4 = [-7, -4, -10, -25, 'no result', -10, 12, 15];
+
+// for (let index = 0; index < temperatures.length; index++) {
+// 	console.log(typeof temperatures[index]);
+// }
+
+// function getAplitude(temps) {
+// 	for (
+// 		let index = 0;
+// 		index < temps.length;
+// 		index++ // 	maxT = temps[index];
+// 	) // if (temps[index] > maxT) {
+// 	// 	console.log(maxT);
+// 	// }
+// 	{
+// 		let temperature = temperatures[index];
+// 		if (typeof temperature === 'number') {
+// 			console.log('number found');
+// 		}
+// 	}
+// }
+
+function calcTempAmplitude(temps1, temps2) {
+	const temps = temps1.concat(temps2);
+	let maxT = temps[0];
+	let minT = temps[0];
+
+	for (let index = 0; index < temps.length; index++) {
+		let currentTemp = temps[index];
+		if (typeof currentTemp !== 'number') continue;
+		if (currentTemp > maxT) maxT = currentTemp;
+		if (currentTemp < minT) minT = currentTemp;
+	}
+	if (minT < 0 && maxT < 0) {
+		minT *= -1;
+		maxT *= -1;
+		return minT - maxT;
+	} else {
+		return maxT - minT;
+	}
+}
+
+const amplitude = calcTempAmplitude(temperatures, temperatures2);
+const amplitude2 = calcTempAmplitude(temperatures3, temperatures4);
+
+console.log(`First amplitude is: ${amplitude}, 
+The second amplitude is: ${amplitude2}`);
