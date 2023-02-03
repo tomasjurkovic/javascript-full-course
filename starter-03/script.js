77611d2dc5a1f3b436dd4175cfa00b31f446b74c
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-03 10:44:37 +0100

    basic code added to see impact of tools installed

--- starter-03/script.js
+++ starter-03/script.js
@@ -1,3 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+if (x === 23) {
+	console.log('x = 23');
+}
+const calcAge = birthyear => 2023 - birthyear;
+
+console.log();
