76c5f86496bcc3c7e480a5049ca6a45e4234f95c
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-07 13:36:28 +0100

    Update script.js
    some basic selectors which will be used later

--- starter-04/script.js
+++ starter-04/script.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct number!';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.score').textContent = 10;
+document.querySelector('.number').textContent = 13;
+
+document.querySelector('.guess').value = 40;
+console.log(document.querySelector('.guess').value);
