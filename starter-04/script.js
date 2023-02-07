f3625a45cf0dea03fde075752bf21bc10f8ebf02
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-07 14:45:38 +0100

    Update script.js
    added first logic

--- starter-04/script.js
+++ starter-04/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct number!';
@@ -11,3 +12,15 @@ document.querySelector('.number').textContent = 13;
 
 document.querySelector('.guess').value = 40;
 console.log(document.querySelector('.guess').value);
+*/
+
+// second argument is anonymous function that write inserted value to console
+// it should be also like this ', function() {same code;}'
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
