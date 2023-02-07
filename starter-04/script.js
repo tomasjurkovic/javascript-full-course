f2fffa8ae94deacd9285f2ed5a50181a5d83ea8d
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-07 15:47:18 +0100

    Update script.js
    some logic applied there

--- starter-04/script.js
+++ starter-04/script.js
@@ -16,11 +16,36 @@ console.log(document.querySelector('.guess').value);
 
 // second argument is anonymous function that write inserted value to console
 // it should be also like this ', function() {same code;}'
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber);
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lose the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lose the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
