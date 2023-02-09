23276e821a500be35ac6e4b38da4eb574d18020d
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-09 17:31:01 +0100

    Update script.js
    deleted misleading comments

--- starter-05/script.js
+++ starter-05/script.js
@@ -5,14 +5,6 @@ const closeModalBtn = document.querySelector('.close-modal');
 const overlayEl = document.querySelector('#overlay');
 const modalEl = document.querySelector('#modal');
 
-// openModalBtns.addEventListener('click', () => {
-//   console.log('click');
-//   //   console.log(e);
-//   //   modalEl.setAttribute('class', '.show-modal');
-//   //   modalEl.setAttribute('class', '.overlay');
-//   //   modalEl.setAttribute('class', '.modal');
-// });
-
 for (let i = 0; i < openModalBtns.length; i++) {
   openModalBtns[i].addEventListener('click', function () {
     console.log('button click');
