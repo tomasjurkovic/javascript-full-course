4010de49847943673e6cc95bdc3bb33d548125c9
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-09 10:34:45 +0100

    again button functionality is done
    coding challenge done

--- starter-04/script.js
+++ starter-04/script.js
@@ -17,7 +17,7 @@ console.log(document.querySelector('.guess').value);
 // second argument is anonymous function that write inserted value to console
 // it should be also like this ', function() {same code;}'
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 let score = 20;
 
@@ -63,4 +63,27 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.number').textContent = secretNumber;
     }
   }
+});
+
+// Coding Challenge #1
+/*
+Implement a game rest functionality, so that the player can make a new guess!
+Your tasks:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the 'score' and
+'secretNumber' variables
+3. Restore the initial conditions of the message, number, score and guess input
+fields
+4. Also restore the original background color (#222) and number width (15rem)
+GOOD LUCK � */
+
+document.querySelector('.again').addEventListener('click', () => {
+  document.querySelector('body').style.backgroundColor = '#222';
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
 });
