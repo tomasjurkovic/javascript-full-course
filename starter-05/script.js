314e97889ac6fbb31c9787acfb277d97c7226cb5
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-09 17:37:42 +0100

    added functions to use them instead of long blocks of code

--- starter-05/script.js
+++ starter-05/script.js
@@ -5,15 +5,18 @@ const closeModalBtn = document.querySelector('.close-modal');
 const overlayEl = document.querySelector('#overlay');
 const modalEl = document.querySelector('#modal');
 
-for (let i = 0; i < openModalBtns.length; i++) {
-  openModalBtns[i].addEventListener('click', function () {
-    console.log('button click');
-    modalEl.classList.remove('hidden'); // not use dot notation here
-    overlayEl.classList.remove('hidden');
-  });
-}
+const openModal = function () {
+  modalEl.classList.remove('hidden'); // not use dot notation here
+  overlayEl.classList.remove('hidden');
+};
 
-closeModalBtn.addEventListener('click', () => {
+const closeModal = function () {
   modalEl.classList.add('hidden'); // not use dot notation here
   overlayEl.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < openModalBtns.length; i++) {
+  openModalBtns[i].addEventListener('click', openModal);
+}
+
+closeModalBtn.addEventListener('click', closeModal);
