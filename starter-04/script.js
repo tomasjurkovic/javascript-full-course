b409523d065a17d7ce5ff5f8ff726f945b89b6d3
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-09 09:40:53 +0100

    Update script.js
    manipulation of css elements

--- starter-04/script.js
+++ starter-04/script.js
@@ -18,34 +18,49 @@ console.log(document.querySelector('.guess').value);
 // it should be also like this ', function() {same code;}'
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 console.log(secretNumber);
 let score = 20;
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
+  // when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+
+    // when user wins
   } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
       score--;
       document.querySelector('.score').textContent = score;
+
+      // when user loses
     } else {
       document.querySelector('.message').textContent = 'You lose the game';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('.number').textContent = secretNumber;
     }
+
+    // when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
       score--;
       document.querySelector('.score').textContent = score;
+
+      // when user loses
     } else {
       document.querySelector('.message').textContent = 'You lose the game';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('.number').textContent = secretNumber;
     }
   }
 });
