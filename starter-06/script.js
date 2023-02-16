8de89619640744a56a23d6b2275f0a113e871d9b
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-16 16:00:47 +0100

    added some enhancements to roll the dice function

--- starter-06/script.js
+++ starter-06/script.js
@@ -8,14 +8,15 @@ const diceEl = document.querySelector('.dice');
 const rollDiceBtn = document.querySelector('.btn--roll');
 const holdBtn = document.querySelector('.btn--hold');
 const newGameBtn = document.querySelector('.btn--new');
-const player1 = document.querySelector('.player--0');
-const player2 = document.querySelector('.player--1');
+const player1 = document.querySelector('#section-player--0');
+const player2 = document.querySelector('#section-player--1');
 
 let defaultTotalScoreP1 = 0;
 let defaultTotalScoreP2 = 0;
 let defaultCurrentScoreP1 = 0;
 let defaultCurrentScoreP2 = 0;
 let winningScore = 100;
+let currentScore = 0;
 
 totalScoreP1El.textContent = 0;
 totalScoreP2El.textContent = 0;
@@ -29,5 +30,32 @@ const startNewGame = function () {
   totalScoreP2El.textContent = defaultTotalScoreP2;
   // TODO: active section
 };
+
+rollDiceBtn.addEventListener('click', function () {
+  // generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // check if rolled one (if yes switch to new player)
+
+  if (dice !== 1) {
+    currentScore += dice;
+    currentScoreP1El.textContent = currentScore;
+  } else {
+    currentScoreP1El.textContent = defaultCurrentScoreP1;
+    currentScore = defaultCurrentScoreP1;
+  }
+  //   if (dice === 1) {
+  //     if (player1.classList.contains('player--active'))
+  //       player1.classList.remove('player--active');
+  //     player2.classList.add('player--active');
+  //   } else {
+  //     player2.classList.remove('player--active');
+  //     player1.classList.add('player--active');
+  //   }
+});
 
 newGameBtn.addEventListener('click', startNewGame);
