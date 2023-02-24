0f33093dc9f83e76c0727a550d83accd007372fe
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-02-24 15:43:10 +0100

    progress

--- starter-behind-the-scenes/script.js
+++ starter-behind-the-scenes/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+  function printAge() {
+    const output = `You are ${age} years old and born in ${birthYear}.`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+      console.log(millenial);
+    }
+    console.log(millenial); // possible, because var are function scope variables
+    // console.log(str); // script.js:13 Uncaught ReferenceError: str is not defined
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Tomas';
+calcAge(1993);
