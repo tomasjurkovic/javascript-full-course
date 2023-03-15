bf5f4a27f5e88f1173d7f93480276f1258d301c1
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-03-15 13:38:37 +0100

    Update script.js

--- starter-behind-the-scenes/this_keyword/script.js
+++ starter-behind-the-scenes/this_keyword/script.js
@@ -1,41 +1,72 @@
 'use strict';
 
-console.log(this); // window object
+// console.log(this); // window object
 
-const calcAge = function (birthYear) {
-  console.log(2023 - birthYear);
-  console.log(this); // it is now undefined
-};
+// const calcAge = function (birthYear) {
+//   console.log(2023 - birthYear);
+//   console.log(this); // it is now undefined
+// };
 
-calcAge(2003);
+// calcAge(2003);
 
-const calcAgeArrow = birthYear => {
-  console.log(2023 - birthYear);
-  console.log(this); // it is window, because arrow function uses
-  // lexical this word, now the parent is window here/
-};
+// const calcAgeArrow = birthYear => {
+//   console.log(2023 - birthYear);
+//   console.log(this); // it is window, because arrow function uses
+//   // lexical this word, now the parent is window here/
+// };
 
-calcAgeArrow(2001);
+// calcAgeArrow(2001);
 
-const tomas = {
-  year: 1992,
+// const tomas = {
+//   year: 1992,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2023 - this.year);
+//   },
+// };
+
+// tomas.calcAge();
+
+// const matilda = {
+//   year: 2012,
+// };
+
+// // method borroing:
+// matilda.calcAge = tomas.calcAge;
+
+// matilda.calcAge(); // this keyword looks like matilda object
+
+// const f = tomas.calcAge;
+// f(); // this is now undefined
+// // Cannot read properties of undefined (reading 'year')
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
   calcAge: function () {
-    console.log(this);
     console.log(2023 - this.year);
+
+    // solution 1:
+    // const self = this; // self or that
+    // const isMillenial = function () {
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+    // isMillenial();
+
+    // solution 2: with arroe functions:
+    const isMillenial = () => {
+      console.log(this); // because of arrow function it is Jonas' object
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
   },
+  greet: function () {
+    console.log(`Hey ${this.firstName}`);
+  },
+  //   greet: () => console.log(`Hey ${this.firstName}`), // newver use arrow function as a method
 };
 
-tomas.calcAge();
-
-const matilda = {
-  year: 2012,
-};
-
-// method borroing:
-matilda.calcAge = tomas.calcAge;
-
-matilda.calcAge(); // this keyword looks like matilda object
-
-const f = tomas.calcAge;
-f(); // this is now undefined
-// Cannot read properties of undefined (reading 'year')
+jonas.greet(); // Hey undefined appears- with arrow function
+// jonas is just object and parent object is window, so we get undefined
+// with normal function it is Hey Jonas
+jonas.calcAge();
