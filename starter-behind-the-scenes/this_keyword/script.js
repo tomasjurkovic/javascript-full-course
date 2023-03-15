146aff7b9309c32ea53b3b2ef6141abe8d846cfc
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-03-15 10:49:14 +0100

    created first examples of using this keyword
    and what this actually is in different situations

--- starter-behind-the-scenes/this_keyword/script.js
+++ starter-behind-the-scenes/this_keyword/script.js
@@ -1,1 +1,28 @@
-'use-strict';
+'use strict';
+
+console.log(this); // window object
+
+const calcAge = function (birthYear) {
+  console.log(2023 - birthYear);
+  console.log(this); // it is now undefined
+};
+
+calcAge(2003);
+
+const calcAgeArrow = birthYear => {
+  console.log(2023 - birthYear);
+  console.log(this); // it is window, because arrow function uses
+  // lexical this word, now the parent is window here/
+};
+
+calcAgeArrow(2001);
+
+const tomas = {
+  year: 1992,
+  calcAge: function () {
+    console.log(this);
+    console.log(2023 - this.year);
+  },
+};
+
+tomas.calcAge();
