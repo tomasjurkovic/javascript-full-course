b41b280a63995c0ef06c3a97a5c40c02a3632816
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-03-15 13:50:18 +0100

    arguments exercise added

--- starter-behind-the-scenes/this_keyword/script.js
+++ starter-behind-the-scenes/this_keyword/script.js
@@ -1,44 +1,44 @@
 'use strict';
 
-// console.log(this); // window object
+console.log(this); // window object
 
-// const calcAge = function (birthYear) {
-//   console.log(2023 - birthYear);
-//   console.log(this); // it is now undefined
-// };
+const calcAge = function (birthYear) {
+  console.log(2023 - birthYear);
+  console.log(this); // it is now undefined
+};
 
-// calcAge(2003);
+calcAge(2003);
 
-// const calcAgeArrow = birthYear => {
-//   console.log(2023 - birthYear);
-//   console.log(this); // it is window, because arrow function uses
-//   // lexical this word, now the parent is window here/
-// };
+const calcAgeArrow = birthYear => {
+  console.log(2023 - birthYear);
+  console.log(this); // it is window, because arrow function uses
+  // lexical this word, now the parent is window here/
+};
 
-// calcAgeArrow(2001);
+calcAgeArrow(2001);
 
-// const tomas = {
-//   year: 1992,
-//   calcAge: function () {
-//     console.log(this);
-//     console.log(2023 - this.year);
-//   },
-// };
+const tomas = {
+  year: 1992,
+  calcAge: function () {
+    console.log(this);
+    console.log(2023 - this.year);
+  },
+};
 
-// tomas.calcAge();
+tomas.calcAge();
 
-// const matilda = {
-//   year: 2012,
-// };
+const matilda = {
+  year: 2012,
+};
 
-// // method borroing:
-// matilda.calcAge = tomas.calcAge;
+// method borroing:
+matilda.calcAge = tomas.calcAge;
 
-// matilda.calcAge(); // this keyword looks like matilda object
+matilda.calcAge(); // this keyword looks like matilda object
 
-// const f = tomas.calcAge;
-// f(); // this is now undefined
-// // Cannot read properties of undefined (reading 'year')
+const f = tomas.calcAge;
+f(); // this is now undefined
+// Cannot read properties of undefined (reading 'year')
 
 const jonas = {
   firstName: 'Jonas',
@@ -70,3 +70,21 @@ jonas.greet(); // Hey undefined appears- with arrow function
 // jonas is just object and parent object is window, so we get undefined
 // with normal function it is Hey Jonas
 jonas.calcAge();
+
+// using arrow function as a function in a method is good way
+
+// arguments keywordL
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+
+addExpr(1, 5); // Arguments(2) [1, 5, callee: (...), Symbol(Symbol.iterator): ƒ]
+addExpr(1, 5, 7, 9); // Arguments(4) [1, 5, 7, 9, callee: (...), Symbol(Symbol.iterator): ƒ]
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+
+addArrow(1, 2); // returns script.js:86 Uncaught ReferenceError: arguments is not defined at addArrow
