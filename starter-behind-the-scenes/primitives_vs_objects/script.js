2b8029914ed7dd44c007073e03166c13e5b7ccbf
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-03-22 13:00:13 +0100

    Update script.js
    added first difference

--- starter-behind-the-scenes/primitives_vs_objects/script.js
+++ starter-behind-the-scenes/primitives_vs_objects/script.js
@@ -1,1 +1,22 @@
 'use-strict';
+
+// primitives:
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+// objects:
+
+const me = {
+  name: 'Tomas',
+  age: 30,
+};
+
+const friend = me;
+
+friend.age = 40;
+
+console.log('Friend:', friend);
+console.log('Me:', me);
