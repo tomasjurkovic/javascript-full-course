4c9e5df250dc13a3252e6eeca55a36ec33486876
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-03-24 11:03:13 +0100

    Update script.js
    uncommented code back

--- starter-data-structures/script.js
+++ starter-data-structures/script.js
@@ -85,74 +85,74 @@ const {
 } = openingHours;
 console.log(from, to); // 11 23 returns
 
-// const arr = [1, 3, 5];
-// const a = arr[0]; // 1
-// const b = arr[1]; // 3
-// const c = arr[2]; // 5
+const arr = [1, 3, 5];
+const a = arr[0]; // 1
+const b = arr[1]; // 3
+const c = arr[2]; // 5
 
-// // destructuring arrays:
-// const [x, y, z] = arr;
-// console.log(x, y, z); // prints 1 3 5
-// // this is very easy how to declare variables
-// // create 3 variables and each will get different value from array, from left to right
-// // original array is not affected at all:
-// console.log(arr);
+// destructuring arrays:
+const [x, y, z] = arr;
+console.log(x, y, z); // prints 1 3 5
+// this is very easy how to declare variables
+// create 3 variables and each will get different value from array, from left to right
+// original array is not affected at all:
+console.log(arr);
 
-// // what if more values?
-// const arr2 = [4, 8, 16];
-// const [i, j, k, l] = arr2;
-// console.log(i, j, k, l); // l is undefined = 4 8 16 undefined
-// console.log(arr2);
+// what if more values?
+const arr2 = [4, 8, 16];
+const [i, j, k, l] = arr2;
+console.log(i, j, k, l); // l is undefined = 4 8 16 undefined
+console.log(arr2);
 
-// // what if less values?
-// const arr3 = [3, 9, 27, 81];
-// const [p, r] = arr3;
-// // it gets values only from first 2 elements of an array
-// console.log(p, r);
-// console.log(arr3); // not affected at all (4) [3, 9, 27, 81]
+// what if less values?
+const arr3 = [3, 9, 27, 81];
+const [p, r] = arr3;
+// it gets values only from first 2 elements of an array
+console.log(p, r);
+console.log(arr3); // not affected at all (4) [3, 9, 27, 81]
 
-// // lets get back to restaurant:
-// const [first, second] = restaurant.categories;
-// console.log(first, second); // prints first two: Italian Pizzeria
+// lets get back to restaurant:
+const [first, second] = restaurant.categories;
+console.log(first, second); // prints first two: Italian Pizzeria
 
-// // what if I need only fist and third?
-// // just leave an empty space:
-// const [thefirst, , thethird] = restaurant.categories;
-// console.log(thefirst, thethird); // prints first and trhird Italian Vegetarian
+// what if I need only fist and third?
+// just leave an empty space:
+const [thefirst, , thethird] = restaurant.categories;
+console.log(thefirst, thethird); // prints first and trhird Italian Vegetarian
 
-// // what if we wanna switch main and secondary categories?
-// let [main, , secondary] = restaurant.categories;
-// console.log(main, secondary);
+// what if we wanna switch main and secondary categories?
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
 
-// // first solution:
-// // const temp = main;
-// // main = secondary;
-// // secondary = temp;
-// // console.log(main, secondary); // prints Vegetarian Italian
-
-// // better solution:
-// [main, secondary] = [secondary, main];
-// // in one line we just change both variables and added them their values but reversed
+// first solution:
+// const temp = main;
+// main = secondary;
+// secondary = temp;
 // console.log(main, secondary); // prints Vegetarian Italian
 
-// console.log(restaurant.order(2, 0)); // prints ['Bruschetta', 'Risotto']
-// //this is how we can do it with destructuring:
-// const [starter, mainCourse] = restaurant.order(3, 1);
-// console.log(starter, mainCourse);
+// better solution:
+[main, secondary] = [secondary, main];
+// in one line we just change both variables and added them their values but reversed
+console.log(main, secondary); // prints Vegetarian Italian
 
-// // nested destructuring:
-// const nested = [1, 2, [5, 6]];
-// const [var1, , var2] = nested;
-// console.log(var1, var2); // prints 1 (2) [5, 6]
-// // var1 = 1
-// // var2 = [5, 6]
+console.log(restaurant.order(2, 0)); // prints ['Bruschetta', 'Risotto']
+//this is how we can do it with destructuring:
+const [starter, mainCourse] = restaurant.order(3, 1);
+console.log(starter, mainCourse);
 
-// // destructuring inside destructuring:
-// const [des1, , [des3, des4]] = nested;
-// console.log(des1, des3, des4); // prints 1 5 6, because we destructured it
+// nested destructuring:
+const nested = [1, 2, [5, 6]];
+const [var1, , var2] = nested;
+console.log(var1, var2); // prints 1 (2) [5, 6]
+// var1 = 1
+// var2 = [5, 6]
 
-// // default values:
-// // useful when we do not know the lenght of the array
-// const [q = 1, pi = 1, ar = 1] = [8, 9];
-// console.log(q, pi, ar);
-// // prints 8 9 1
+// destructuring inside destructuring:
+const [des1, , [des3, des4]] = nested;
+console.log(des1, des3, des4); // prints 1 5 6, because we destructured it
+
+// default values:
+// useful when we do not know the lenght of the array
+const [q = 1, pi = 1, ar = 1] = [8, 9];
+console.log(q, pi, ar);
+// prints 8 9 1
