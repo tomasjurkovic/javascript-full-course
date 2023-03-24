ce9d1022f05e86b83e74c8502d4a27fcc4f1f0a7
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-03-24 09:36:01 +0100

    Update script.js
    other examples of destructuring

--- starter-data-structures/script.js
+++ starter-data-structures/script.js
@@ -53,3 +53,27 @@ const [p, r] = arr3;
 // it gets values only from first 2 elements of an array
 console.log(p, r);
 console.log(arr3); // not affected at all (4) [3, 9, 27, 81]
+
+// lets get back to restaurant:
+const [first, second] = restaurant.categories;
+console.log(first, second); // prints first two: Italian Pizzeria
+
+// what if I need only fist and third?
+// just leave an empty space:
+const [thefirst, , thethird] = restaurant.categories;
+console.log(thefirst, thethird); // prints first and trhird Italian Vegetarian
+
+// what if we wanna switch main and secondary categories?
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// first solution:
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary); // prints Vegetarian Italian
+
+// better solution:
+[main, secondary] = [secondary, main];
+// in one line we just change both variables and added them their values but reversed
+console.log(main, secondary); // prints Vegetarian Italian
