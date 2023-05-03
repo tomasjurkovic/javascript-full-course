874f324d216ec0c1d91e977788047f829a2068ce
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-05-03 12:52:11 +0200

    first 2 steps finished

--- starter-data-structures/coding_challenges.js
+++ starter-data-structures/coding_challenges.js
@@ -28,5 +28,58 @@
 // Then, call the function again with players from game.scored
 
 // STEP 1:
-const players1 = [];
-const players2 = [];
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+        ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+// step 1:
+const players1 = game.players[0];
+const players2 = game.players[1];
+
+// step 2: 
+// const gk1 = 
+
+const [gk1, ...fieldPlayers1] = players1;
+const [gk2, ...fieldPlayers2] = players2;
+
+console.log(gk1, fieldPlayers1);
+console.log(fieldPlayers2);
