8b6bc3116a3a839554a7b5ae5f86265f6fdebb28
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-05-15 11:09:30 +0200

    Update coding_challenges.js
    first task is done

--- starter-data-structures/coding_challenges.js
+++ starter-data-structures/coding_challenges.js
@@ -229,3 +229,40 @@ for (const player of game.scored) {
 }
 
 console.log(scorers2); // prints: {Lewandowski: 2, Gnabry: 1, Hummels: 1}
+
+// Coding challenge #3:
+// Let's continue with our football betting app! This time, we have a map called 
+// 'gameEvents' (see below) with a log of the events that happened during the 
+// game. The values are the events themselves, and the keys are the minutes in which 
+// each event happened (a football game has 90 minutes plus some extra time).
+// Your tasks:
+// 1. Create an array 'events' of the different game events that happened (no 
+// duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64 
+// was unfair. So remove this event from the game events log.
+// 3. Compute and log the following string to the console: "An event happened, on 
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over 'gameEvents' and log each element to the console, marking 
+// whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽ GOAL
+// GOOD LUCK �
+
+ const gameEvents = new Map([
+ [17, '⚽ GOAL'],
+ [36, '� Substitution'],
+ [47, '⚽ GOAL'],
+ [61, '� Substitution'],
+ [64, '� Yellow card'],
+ [69, '� Red card'],
+ [70, '� Substitution'],
+ [72, '� Substitution'],
+ [76, '⚽ GOAL'],
+ [80, '⚽ GOAL'],
+ [92, '� Yellow card'],
+ ]);
+ 
+ // task 1:
+ const events = new Set(gameEvents.values());
+ console.log(events); 
+ // I converted this to set, so I got only the unique values with no duplicates
+ // it prints: {'⚽ GOAL', '� Substitution', '� Yellow card', '� Red card'}
