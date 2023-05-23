eec38b8ef3c6fdb5bb2a92a336077312c309dbd8
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-05-23 09:40:22 +0200

    progress so far
    first example

--- starter-functions/script.js
+++ starter-functions/script.js
@@ -116,3 +116,22 @@ function count() {
     }
 }
 // this returns a function 
+
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS:
+
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function(str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Transform by: ${fn.name}`); // functions can have properties
+}
+
+transformer('JavaScript is the best', upperFirstWord);
