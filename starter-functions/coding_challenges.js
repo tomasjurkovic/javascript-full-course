91ea0d756880ec41a69f5777047f2130b43d63cc
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-05-30 15:51:03 +0200

    Update coding_challenges.js
    coding challenge completed

--- starter-functions/coding_challenges.js
+++ starter-functions/coding_challenges.js
@@ -44,19 +44,27 @@ const poll = {
     options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
     // This generates [0, 0, 0, 0]. More in the next section!
     answers: new Array(4).fill(0),
+   
     registerNewAnswer() {
         // get an answer:
         const answer = Number(
             prompt(
                 `${this.question}\n${this.options.join('\n')}\nWrite your number.`
             ));
-
         // update the answers:
         typeof answer === 'number' && 
             answer < this.answers.length && 
             this.answers[answer]++;
     
-        console.log(this.answers);
+        this.displayResults();
+        this.displayResults('string');
+    },
+    displayResults(type = 'array') {
+        if (typeof type === 'array') {
+            console.log(this.answers);
+        } else if (typeof type === 'string') {
+            console.log(`Poll results are ${this.answers.join(', ')}`);
+        }
     },
 };
 
@@ -65,3 +73,19 @@ document
     .querySelector('.poll')
     .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
+// task 3:
+// 3. Create a method 'displayResults' which displays the poll results. The
+// method takes a string as an input (called 'type'), which can be either 'string'
+// or 'array'. If type is 'array', simply display the results array as it is, using
+// console.log(). This should be the default option. If type is 'string', display a
+// string like "Poll results are 13, 2, 4, 1".
+
+//task 4
+// 4. Run the 'displayResults' method at the end of each
+// 'registerNewAnswer' method call.
+
+// Test data for bonus:
+// § Data 1: [5, 2, 3]
+// § Data 2: [1, 5, 3, 9, 6, 1]
+
+poll.displayResults.call({answers: [5, 2, 3]})
