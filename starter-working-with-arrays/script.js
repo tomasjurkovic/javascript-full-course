4a6f534636d16a75615f32030d27732e48cb2972
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-06-14 14:33:19 +0200

    Update script.js

--- starter-working-with-arrays/script.js
+++ starter-working-with-arrays/script.js
@@ -269,3 +269,15 @@ for (const mov of movements) {
 
 console.log(movementsUsdForOf);
 // prints[220.00000000000003, 495.00000000000006, -440.00000000000006, 3300.0000000000005, -715.0000000000001, -143, 77, 1430.0000000000002]
+
+// map method has access to index and whole array
+// it is okay to have two or more return statements
+const movementsDescription = movements.map((mov, i) =>
+  `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${mov}$`
+);
+
+console.log(movementsDescription);
+// prints ['Movement 1: You deposited 200$', 'Movement 2: You deposited 450$', 
+// 'Movement 3: You withdrew -400$', 'Movement 4: You deposited 3000$', 
+// 'Movement 5: You withdrew -650$', 'Movement 6: You withdrew -130$', 
+// 'Movement 7: You deposited 70$', 'Movement 8: You deposited 1300$']
