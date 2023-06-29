45747300ba54ddc80a9b4eb1496d0021430f14b0
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-06-29 14:30:22 +0200

    solution added

--- starter-working-with-arrays/coding_challenges.js
+++ starter-working-with-arrays/coding_challenges.js
@@ -109,3 +109,15 @@ Test data:
 § Data 1: [5, 2, 4, 1, 15, 8, 3]
 § Data 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK */
+
+const calcAverageHumanAgeChaining = (dogsAge) => {
+  const averageAdultDogAge = dogsAge
+    .map(age => age = 2 ? age * 2 : 16 + age * 4)
+    .filter(humanAge => humanAge >= 18)
+    .reduce((acc, cur, i, arr) => 
+      acc + cur / arr.length, 0);
+    return averageAdultDogAge;
+}
+
+console.log(calcAverageHumanAgeChaining(dogs1));
+console.log(calcAverageHumanAgeChaining(dogs2));
