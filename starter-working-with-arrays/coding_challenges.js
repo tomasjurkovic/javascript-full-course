6e3bdcf1c0a47539368f49c30f41912e090f5113
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-06-29 14:17:43 +0200

    instructions added

--- starter-working-with-arrays/coding_challenges.js
+++ starter-working-with-arrays/coding_challenges.js
@@ -103,3 +103,9 @@ const calcAverageHumanAge = function (dogsAge) {
 console.log(calcAverageHumanAge(dogs1));
 console.log(calcAverageHumanAge(dogs2));
 
+/* Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time 
+as an arrow function, and using chaining!
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK */
