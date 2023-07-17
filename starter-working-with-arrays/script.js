72c77f0de0fe35aa8c908146acbbd86c272f969f
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-07-17 15:49:28 +0200

    done sorting

--- starter-working-with-arrays/script.js
+++ starter-working-with-arrays/script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements) {
+const displayMovements = function(movements, sort = false) {
   containerMovements.innerHTML = '';
   // deletes existing content...
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
     const html = `
     <div class="movements__row">
@@ -196,6 +198,13 @@ btnLoan.addEventListener('click', function (e) {
     }
   // clearing input loan form field:  
   inputLoanAmount.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted; // give it opposite state after clicking on sorting button
 });
 
 /////////////////////////////////////////////////
