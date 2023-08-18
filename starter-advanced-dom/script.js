186f984395a91671c08df302b73e59602462e9a9
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-08-18 14:20:05 +0200

    deleting element

--- starter-advanced-dom/script.js
+++ starter-advanced-dom/script.js
@@ -56,11 +56,15 @@ const allBtns = document.getElementsByClassName('btn');
 console.log(allBtns); //refresh automatically 
 
 // creating and inserting elements:
-// .insertAdjacentHTM 
+// creating element itself:
 const message = document.createElement('div'); // not yet in the DOM itself
 message.classList.add('cookie-message');
 // message.textContent = 'We use cookies for improveing functionality and analytics.';
 message.innerHTML = 'We use cookies for improveing functionality and analytics. <button class ="btn btn--close-cookie"> Got it! </button>';
+
+// inserting element:
+// .insertAdjacentHTM 
+// header.insertAdjacentElement('beforeend', message); // example of doing it
 
 // header.prepend(message); // displayed as a first element
 header.append(message); // it just moved it down if line above is applied
@@ -70,3 +74,11 @@ header.append(message); // it just moved it down if line above is applied
 // header.before(message);
 // header.after(message);
 // those two will make a message element a sibling to the header element
+
+// removing cookie element when clicking on button:
+document.querySelector('.btn--close-cookie')
+  .addEventListener('click', function() {
+    message.remove(); // not needed to select it again, when we have a variable
+    // previously:
+    // message.parentElement.removeChild(message);
+});
