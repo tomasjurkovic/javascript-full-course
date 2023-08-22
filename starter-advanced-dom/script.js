85e760043326920891f5eebf8895fa909bd5c127
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-08-22 16:14:46 +0200

    using event delegation better way

--- starter-advanced-dom/script.js
+++ starter-advanced-dom/script.js
@@ -68,15 +68,29 @@ btnScrollTo.addEventListener('click', function(e) {
   section1.scrollIntoView({behavior: 'smooth'});
 });
 
-// page navigation:
-document.querySelectorAll('.nav__link')
-  .forEach(function(el) {
-    el.addEventListener('click', function(e) {
+// page navigation: before refactor:
+// document.querySelectorAll('.nav__link')
+//   .forEach(function(el) {
+//     el.addEventListener('click', function(e) {
+//       e.preventDefault();
+//       const id = this.getAttribute('href');
+//       document.querySelector(id)
+//         .scrollIntoView({behavior: 'smooth'});
+//     });
+// });
+
+// page navigation: event handler:
+// 1. add event listener to common parent element,
+// 2. determine which element originated the event...
+document.querySelector('.nav__links')
+  .addEventListener('click', function(e) {
+    // Matching strategy:
+    if (e.target.classList.contains('nav__link')) {
       e.preventDefault();
-      const id = this.getAttribute('href');
+      const id = e.target.getAttribute('href');
       document.querySelector(id)
         .scrollIntoView({behavior: 'smooth'});
-    });
+    }
 });
 
 /////////////////////////////////////////////////////
