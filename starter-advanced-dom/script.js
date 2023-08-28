77b666ff773620b1ee45c5af84a1e731ab060b62
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-08-28 10:45:50 +0200

    images part done

--- starter-advanced-dom/script.js
+++ starter-advanced-dom/script.js
@@ -277,6 +277,37 @@ featureIcons.forEach(icon => featureIconsObserver.observe(icon));
 
 // slider:
 const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach((s, i) => 
+    (s.style.transform = `translateX(${100 * (i - slide)}%)`));
+};
+
+const nextSlide = function() {
+  if(curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+}
+
+const previousSlide = function() {
+  if(curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+}
+
+btnRight.addEventListener('click', nextSlide);
+
+btnLeft.addEventListener('click', previousSlide);
 
 const slider = document.querySelector('.slider');
 slider.style.transform = 'scale(0.4) translateX(-1200px)';
