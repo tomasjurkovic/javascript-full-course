58a4837677e6e936013c4afdb09b3869e1aea9e5
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-08-31 11:25:15 +0200

    constructor example created

--- starter-oop/script.js
+++ starter-oop/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// constructors start with Capital letter always:
+const Person = function(firstName, birthYear) {
+    // console.log(this); // Person {}
+    this.firstName = firstName; // convention
+    this.birthYear = birthYear;
+};
+
+// call constructor function with new operator:
+// behind the scene:
+// 1. new {} is created
+// 2. function is called, this keyword = {}
+// 3. {} is linked to prototype
+// 4. function automatically return {} or created object itself
+const tomas = new Person('Tomas', 1993);
+
+console.log(tomas);
+// prints Person {firstName: 'Tomas', birthYear: 1993}
