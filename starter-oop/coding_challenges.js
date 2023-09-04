4f832e2aa7e04507ee1a5e6708109a9102bae786
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-09-04 11:43:04 +0200

    solution added by myself

--- starter-oop/coding_challenges.js
+++ starter-oop/coding_challenges.js
@@ -12,5 +12,45 @@ the new speed to the console
 4. Create 2 'Car' objects and experiment with calling 'accelerate' and 
 'brake' multiple times on each of them
 Test data:
-§ Data car 1: 'BMW' going at 120 km/h
+§ Data car 1: 'bmw' going at 120 km/h
 § Data car 2: 'Mercedes' going at 95 km/h */
+
+// 1
+const Car = function(make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+// 2
+Car.prototype.accelerate = function() {
+    this.speed += 10;
+    console.log(`The current car's speed is: ${this.speed}`);
+};
+
+// 3
+Car.prototype.brake = function() {
+    this.speed -= 5;
+    console.log(`The current car's speed is: ${this.speed}`);
+};
+
+const bmw = new Car('BMW', 120);
+console.log(bmw);
+
+const mercedes = new Car('Mercedes', 95); 
+console.log(mercedes);
+
+bmw.brake();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+bmw.accelerate();
+
+mercedes.brake();
+mercedes.brake();
+mercedes.brake();
+mercedes.brake();
+mercedes.accelerate();
+mercedes.brake();
+mercedes.brake();
+mercedes.accelerate();
+mercedes.brake();
