b6da44588e18bffeecd024ae1fc452d1e8bec1ad
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-09-05 10:37:58 +0200

    my solutions

--- starter-oop/coding_challenges.js
+++ starter-oop/coding_challenges.js
@@ -103,3 +103,71 @@ ford.brake();
 ford.speedUS = 120;
 console.log(ford);
 console.log(ford.speedUS);
+
+/* Coding Challenge #3
+Your tasks:
+1. Use a constructor function to implement an Electric Car (called 'EV') as a child
+"class" of 'Car'. Besides a make and current speed, the 'EV' also has the 
+current battery charge in % ('charge' property)
+2. Implement a 'chargeBattery' method which takes an argument 
+'chargeTo' and sets the battery charge to 'chargeTo'
+3. Implement an 'accelerate' method that will increase the car's speed by 20, 
+and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 
+km/h, with a charge of 22%'
+4. Create an electric car object and experiment with calling 'accelerate', 
+'brake' and 'chargeBattery' (charge to 90%). Notice what happens when 
+you 'accelerate'! Hint: Review the definiton of polymorphism �
+Test data:
+§ Data car 1: 'Tesla' going at 120 km/h, with a charge of 23% */
+
+// 1:
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+// 2:
+EV.prototype.chargeBattery = function (chargeTo) {
+    if(this.charge > chargeTo) {
+        console.log(`You can't charge less battery (${chargeTo}), than it already has ${this.charge}.`);
+        return;
+    };
+    if(chargeTo > 100) {
+        this.charge = 100;
+        return;
+    }
+    this.charge = chargeTo;
+};
+
+// 3: 
+EV.prototype.accelerate = function () {
+    this.charge -= 1;
+    if(this.charge === 0 || this.charge <= 0) {
+        this.charge = 0;
+        console.log(`Battery is ${this.charge}%. Need to charge.`);
+        return;
+    }
+    this.speed += 20;
+    console.log(`${this.make} is going at ${this.speed}, with a charge of ${this.charge}%.`);
+};
+
+// 4:
+const tesla = new EV('Tesla', 120, 23);
+console.log(tesla);
+tesla.accelerate(); // since this is a chain, 
+// it takes the first found method in chain when they have same names.
+tesla.brake();
+tesla.chargeBattery(100);
+tesla.accelerate();
+tesla.brake();
+tesla.brake();
+tesla.chargeBattery(95);
+tesla.chargeBattery(102);
+
+const zhidou = new EV('Zhidou', 100, 2);
+zhidou.accelerate();
+zhidou.accelerate();
+
+// added some bonus checks, looks better now :)
