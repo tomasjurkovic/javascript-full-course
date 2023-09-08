4d2d13352d945343bcdba2d624bb4b4a51685e30
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-09-08 14:49:00 +0200

    added description

--- starter-oop/coding_challenges.js
+++ starter-oop/coding_challenges.js
@@ -171,3 +171,16 @@ zhidou.accelerate();
 zhidou.accelerate();
 
 // added some bonus checks, looks better now :)
+
+/* Coding Challenge #4
+Your tasks:
+1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl'
+child class of the 'CarCl' class
+2. Make the 'charge' property private
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery'
+methods of this class, and also update the 'brake' method in the 'CarCl'
+class. Then experiment with chaining!
+Test data:
+§ Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
+GOOD LUCK  */
+
