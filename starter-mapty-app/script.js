00fc1ca06ab0cb709ef2f033713a951d5f6a42e3
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-09-11 09:37:13 +0200

    Update script.js

--- starter-mapty-app/script.js
+++ starter-mapty-app/script.js
@@ -10,3 +10,22 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// geolocation API:
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position);
+        // using destructuring here:
+        // SAME AS:    position.coords.latitude;
+        // SAME AS:
+        // const {latitude} = position.coords;
+        // const {longitude} = position.coords;
+        const {latitude, longitude} = position.coords;
+        console.log(latitude, longitude);
+
+        // check if google link will open map with my current location:
+        console.log(`https://www.google.sk/maps/@${latitude},${longitude}`);
+    }, function () {
+        alert('Could not get your position');
+    });
+}
