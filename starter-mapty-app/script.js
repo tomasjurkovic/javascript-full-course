04728007c1ec93bdb3f1f3d57846d914a5bfeced
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-09-11 14:53:06 +0200

    Update script.js

--- starter-mapty-app/script.js
+++ starter-mapty-app/script.js
@@ -33,9 +33,24 @@ if (navigator.geolocation) {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker(coords).addTo(map)
-            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-            .openPopup();
+        // special object with couple of methods:
+        map.on('click', function(mapEvent) {
+            console.log(mapEvent);
+
+            const {lat, lng} = mapEvent.latlng;
+            const crds = [lat, lng];
+            L.marker(crds).addTo(map)
+                .bindPopup(L.popup({
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: 'running-popup'
+                    })
+                )
+                .setPopupContent('Workout')
+                .openPopup();
+        })
     }, function () {
         alert('Could not get your position');
     });
