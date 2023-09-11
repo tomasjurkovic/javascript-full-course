edd9a14fe87c893df455b6aab6aff82eefd955df
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-09-11 15:22:11 +0200

    Update script.js

--- starter-mapty-app/script.js
+++ starter-mapty-app/script.js
@@ -10,6 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
 
 // geolocation API:
 if (navigator.geolocation) {
@@ -27,29 +28,37 @@ if (navigator.geolocation) {
         // check if google link will open map with my current location:
         console.log(`https://www.google.sk/maps/@${latitude},${longitude}`);
 
-        const map = L.map('map').setView(coords, 13);
+        map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
         // special object with couple of methods:
-        map.on('click', function(mapEvent) {
-            console.log(mapEvent);
+        // handling clicks on map:
+        map.on('click', function(mapE) {
+            console.log(mapE);
 
-            const {lat, lng} = mapEvent.latlng;
-            const crds = [lat, lng];
-            L.marker(crds).addTo(map)
-                .bindPopup(L.popup({
-                    maxWidth: 250,
-                    minWidth: 100,
-                    autoClose: false,
-                    closeOnClick: false,
-                    className: 'running-popup'
-                    })
-                )
-                .setPopupContent('Workout')
-                .openPopup();
+            // defining global variable:
+            mapEvent = mapE;
+            // removing class hidden from the form after clicking
+            form.classList.remove('hidden');
+            // focusing on next form input (better user experience)
+            inputDistance.focus();
+
+            // const {lat, lng} = mapEvent.latlng;
+            // const crds = [lat, lng];
+            // L.marker(crds).addTo(map)
+            //     .bindPopup(L.popup({
+            //         maxWidth: 250,
+            //         minWidth: 100,
+            //         autoClose: false,
+            //         closeOnClick: false,
+            //         className: 'running-popup'
+            //         })
+            //     )
+            //     .setPopupContent('Workout')
+            //     .openPopup();
         })
     }, function () {
         alert('Could not get your position');
@@ -58,3 +67,29 @@ if (navigator.geolocation) {
 
 // accessing global variable from the other.sj script:
 // console.log(`I am ${firstName}`); // printed 'Tomas'
+
+// add event listener for submitting the form:
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // clear inputs fields:
+    inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
+    const {lat, lng} = mapEvent.latlng;
+    const crds = [lat, lng];
+        L.marker(crds).addTo(map)
+        .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+});
+
+inputType.addEventListener('change', function() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+})
