00ff844909e3189556a2a01335ce252bee204bb0
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-09-11 13:14:50 +0200

    displaying a map

--- starter-mapty-app/script.js
+++ starter-mapty-app/script.js
@@ -21,21 +21,25 @@ if (navigator.geolocation) {
         // const {latitude} = position.coords;
         // const {longitude} = position.coords;
         const {latitude, longitude} = position.coords;
+        const coords = [latitude, longitude];
         console.log(latitude, longitude);
 
         // check if google link will open map with my current location:
         console.log(`https://www.google.sk/maps/@${latitude},${longitude}`);
 
-        const map = L.map('map').setView([latitude, longitude], 13);
+        const map = L.map('map').setView(coords, 13);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([latitude, longitude]).addTo(map)
+        L.marker(coords).addTo(map)
             .bindPopup('A pretty CSS popup.<br> Easily customizable.')
             .openPopup();
     }, function () {
         alert('Could not get your position');
     });
-}
+};
+
+// accessing global variable from the other.sj script:
+// console.log(`I am ${firstName}`); // printed 'Tomas'
