a25913b675be51cfbeb929c4684c60ce9459d8fb
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-09-12 11:04:15 +0200

    changed according required architecture

--- starter-mapty-app/script.js
+++ starter-mapty-app/script.js
@@ -10,17 +10,26 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-let map, mapEvent;
 
 class App {
+    #map;
+    #mapEvent;
     constructor() {
+        this._getPosition(); 
+        // display map while page is loading right at the constructor
 
+        // add event listener for submitting the form:
+        form.addEventListener('submit', this._newWorkout.bind(this)); 
+        // event handler function
+        // there is need to bind it this way to app (using .bind(this))
+
+        inputType.addEventListener('change', this._toggleElevationField);
     }
 
     // geolocation API:
     _getPosition() {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this._loadMap, function () {
+            navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function () {
                 alert('Could not get your position');
             });
         };
@@ -40,63 +49,55 @@ class App {
         // check if google link will open map with my current location:
         console.log(`https://www.google.sk/maps/@${latitude},${longitude}`);
 
-        map = L.map('map').setView(coords, 13);
+        console.log(this);
+        this.#map = L.map('map').setView(coords, 13);
 
         L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(this.#map);
 
         // special object with couple of methods:
         // handling clicks on map:
-        map.on('click', function(mapE) {
-            console.log(mapE);
-
-            // defining global variable:
-            mapEvent = mapE;
-            // removing class hidden from the form after clicking
-            form.classList.remove('hidden');
-            // focusing on next form input (better user experience)
-            inputDistance.focus();
-
-            // const {lat, lng} = mapEvent.latlng;
-            // const crds = [lat, lng];
-            // L.marker(crds).addTo(map)
-            //     .bindPopup(L.popup({
-            //         maxWidth: 250,
-            //         minWidth: 100,
-            //         autoClose: false,
-            //         closeOnClick: false,
-            //         className: 'running-popup'
-            //         })
-            //     )
-            //     .setPopupContent('Workout')
-            //     .openPopup();
-        })
+        this.#map.on('click', this._showForm.bind(this));
     }
 
-    // _showForm(){}
+    _showForm(mapE){
+        // defining global variable:
+        this.#mapEvent = mapE;
+        // removing class hidden from the form after clicking
+        form.classList.remove('hidden');
+        // focusing on next form input (better user experience)
+        inputDistance.focus();
+        
+        // const {lat, lng} = mapEvent.latlng;
+        // const crds = [lat, lng];
+        // L.marker(crds).addTo(map)
+        //     .bindPopup(L.popup({
+        //         maxWidth: 250,
+        //         minWidth: 100,
+        //         autoClose: false,
+        //         closeOnClick: false,
+        //         className: 'running-popup'
+        //         })
+        //     )
+        //     .setPopupContent('Workout')
+        //     .openPopup();
+    }
 
-    // _toggleElevationField(){}
+    _toggleElevationField(){
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
 
-    // _newWorkout(){}
+    _newWorkout(e){
+        e.preventDefault();
+        console.log(this);
 
-};
-
-// create app object
-const app = new App();
-
-// accessing global variable from the other.sj script:
-// console.log(`I am ${firstName}`); // printed 'Tomas'
-
-// add event listener for submitting the form:
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    // clear inputs fields:
-    inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
-    const {lat, lng} = mapEvent.latlng;
-    const crds = [lat, lng];
-        L.marker(crds).addTo(map)
+        // clear inputs fields:
+        inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
+        const {lat, lng} = this.#mapEvent.latlng;
+        const crds = [lat, lng];
+        L.marker(crds).addTo(this.#map)
         .bindPopup(L.popup({
                 maxWidth: 250,
                 minWidth: 100,
@@ -107,9 +108,14 @@ form.addEventListener('submit', function(e) {
         )
         .setPopupContent('Workout')
         .openPopup();
-});
+        
+    }
 
-inputType.addEventListener('change', function() {
-    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+};
+
+// create app object
+const app = new App();
+// app._getPosition(); // better to do it in the constructor
+
+// accessing global variable from the other.sj script:
+// console.log(`I am ${firstName}`); // printed 'Tomas'
