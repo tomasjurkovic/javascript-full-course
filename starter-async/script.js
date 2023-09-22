061fb686f9d3883d4f6f13fb099a92fc93bd033a
tomasjurkovic <tomas.jurkovic@hotmail.com>
2023-09-22 10:32:13 +0200

    example of event loop in action

--- starter-async/script.js
+++ starter-async/script.js
@@ -279,3 +279,19 @@ whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
 // whereAmI(48.71395, 21.25808);
+
+// EVENT LOOP:
+console.log('Test start'); 
+setTimeout(() => console.log('0 sec timer'), 0); 
+Promise.resolve('Resolved promise 1').then(res => console.log(res)); 
+Promise.resolve('Resolved promise 2').then(res => {
+    // just this microtask takes long time
+    for (let i = 0; i < 111111111; i++) {};
+    console.log(res);
+});
+console.log("Test end"); 
+// 1, 5, 3, 4, 2 order
+// top level code goes first
+// promise and timout goes after it / both in callback queues
+// callback of the resolved promise will go to microtask queue
+// it means it has priority over basic callback queue
